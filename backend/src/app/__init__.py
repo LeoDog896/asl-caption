c3f192fc686794adfd38bd7d00bde141032b5bf8
Tristan F <LeoDog896@hotmail.com>
2023-06-16 22:19:25 -0400

    feat: enhanced forms
    contract required for backend to return serializable data.
    
    it could technically *not* do this, but that would require custom deserialization
    which I don't think is too necessary to implement

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 
 from werkzeug.datastructures.file_storage import FileStorage
@@ -32,18 +32,14 @@ def allowed_file(file: FileStorage) -> bool:
 @app.route('/api/upload', methods=['POST'])
 def api_upload():
     if 'file' not in request.files:
-        return 'invalid request: no file part', BAD_REQUEST
+        return jsonify({'error': 'invalid request: no file part'}), 400
     file = request.files['file']
 
     if file.filename == '':
-        return 'invalid request: no file supplied', BAD_REQUEST
+        return jsonify({'error': 'invalid request: no file supplied'}), 400
     if not allowed_file(file):
-        return 'invalid request: unsupported file type', BAD_REQUEST
+        return jsonify({'error': 'invalid request: unsupported file type'}), 400
 
     content = file.stream.read()
     msg = f'Received file "{file.filename}" of type "{file.mimetype}" with size of {len(content)}B!'
-    return Response(
-        response=msg,
-        status=400,
-        content_type='text/plain'
-    )
+    return jsonify({'message': msg}), 200
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
   import { backend } from '$lib/backend';
   import FileUpload from '$lib/component/FileUpload.svelte';
+  import { enhance } from '$app/forms';
 
   export let data: PageData;
 
@@ -20,7 +21,7 @@ Very much a WIP
 Server Message:<code>{data.message}</code>
 
 <h2>File Upload Test</h2>
-<form action={backend('upload')} method="post" enctype="multipart/form-data">
+<form action={backend('upload')} method="post" enctype="multipart/form-data" use:enhance>
   <FileUpload id="file" name="file" bind:file />
 
   <button type="submit">Upload</button>
