86b50826542449c335bdbdb87ce2ff5291f59d05
Tristan F <LeoDog896@hotmail.com>
2023-06-16 23:34:19 -0400

    refactor: use werkzeug constants

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from werkzeug import exceptions
 from werkzeug.utils import secure_filename
-
-# status codes
-
-BAD_REQUEST = 400
 
 # flask setup
 
@@ -27,14 +24,14 @@ def api_ping():
 @app.route('/api/upload', methods=['POST'])
 def api_upload():
     if 'file' not in request.files:
-        return jsonify({'error': 'invalid request: no file part'}), 400
+        return jsonify({'error': 'invalid request: no file part'}), exceptions.BadRequest.code
     file = request.files['file']
 
     if file.filename == '':
-        return jsonify({'error': 'invalid request: no file supplied'}), 400
+        return jsonify({'error': 'invalid request: no file supplied'}), exceptions.BadRequest.code
 
     filename = secure_filename(file.filename)
 
     content = file.stream.read()
     msg = f'Received file "{filename}" of type "{file.mimetype}" with size of {len(content)}B!'
-    return jsonify({'message': msg}), 200
+    return jsonify({'message': msg}), exceptions.OK.code
