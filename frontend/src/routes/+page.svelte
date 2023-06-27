e2bf6ba302280c6102b3c391567d166287a0d5b5
Tristan F <LeoDog896@hotmail.com>
2023-06-27 16:54:33 -0400

    refactor: move landing to separate page component
    Co-Authored-By: gamejam-sandwich <114552117+gamejam-sandwich@users.noreply.github.com>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
-  import FileUpload from '$lib/component/FileUpload.svelte';
-  import { process } from '$lib/model';
-  import { IconCamera, IconFile, IconLoader } from '@tabler/icons-svelte';
-  import * as tf from '@tensorflow/tfjs';
-  import { loadGraphModel } from '@tensorflow/tfjs-converter';
-  import { onMount } from 'svelte';
-  import { getContext } from 'svelte';
-  import type { Context } from 'svelte-simple-modal';
-  const { open } = getContext<Context>('simple-modal');
-  import ImageDisplay from '$lib/component/display/ImageDisplay.svelte';
-  import VideoDisplay from '$lib/component/display/VideoDisplay.svelte';
   import type * as cjs from 'chart.js';
-  import { fly } from 'svelte/transition';
   import People from "$lib/component/page/People.svelte";
 
   // Statistics
   import { Line } from 'svelte-chartjs';
   import 'chart.js/auto';
   import results from '../stats/results.csv';
+  import Landing from '$lib/component/page/Landing.svelte';
+
   const parsedResults = results.map((result) =>
     Object.fromEntries(Object.entries(result).map(([key, value]) => [key.trim(), value.trim()]))
   );
@@ -81,57 +71,6 @@
     labels,
     datasets
   };
-
-  // Model files
-  import MODEL_URL from '../model/model.json?url';
-  const shards = import.meta.glob('../model/*.bin', { as: 'url', eager: true });
-
-  let file: File | undefined;
-  let model: tf.GraphModel<string | tf.io.IOHandler>;
-  let loading = true;
-
-  onMount(async () => {
-    model = await loadGraphModel(
-      tf.io.http(MODEL_URL, {
-        fetchFunc: (url: string, init: Parameters<typeof fetch>[1]) => {
-          // since Vite adds extra hashes to the filenames, we need to transform the requests to the hashed filenames
-          const filename = url.split('/').pop() as string;
-          const shard = shards[`../model/${filename}`];
-          if (shard) {
-            return fetch(shard, init);
-          }
-          return fetch(url, init);
-        }
-      })
-    );
-    loading = false;
-  });
-
-  $: if (file) {
-    if (model) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const img = new Image();
-        img.onload = async () => {
-          const data = await process(model, img);
-
-          open(ImageDisplay, {
-            data
-          });
-        };
-
-        img.src = reader.result as string;
-      };
-
-      reader.readAsDataURL(file);
-    }
-  }
-
-  function openVideo() {
-    open(VideoDisplay, {
-      model
-    });
-  }
 </script>
 
 <svelte:head>
@@ -139,35 +78,7 @@
 </svelte:head>
 
 <main>
-  <div class="landing" transition:fly={{ y: 200, duration: 1000 }}>
-    <h1>See the <span class="gradient">world</span> speak.</h1>
-
-    <h2>
-      We translate <a href="https://www.nidcd.nih.gov/health/american-sign-language">ASL</a> to English
-      in all media.
-    </h2>
-
-    <img src="landing.png" id="hand" alt="Hand doing the ASL pose for 'R'." />
-
-    <div class="container">
-      <div id="upload">
-        <h3 class="small-call">Upload your Media</h3>
-        <div class="buttons">
-          <FileUpload id="file" bind:file>
-            <div class="button"><IconFile />From File</div>
-          </FileUpload>
-          <button class="button" on:click={openVideo} disabled={loading}>
-            {#if loading}
-              <IconLoader class="spin" />
-            {:else}
-              <IconCamera />
-            {/if}
-            Live Camera
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Landing></Landing>
   <div class="slant">
     <h2 id="about">About Us</h2>
 
@@ -301,37 +212,10 @@
     background-color: var(--secondary-hover);
   }
 
-  .small-call {
-    font-size: 35px;
-    margin: 1rem;
-    font-weight: 800;
-  }
-
   footer {
     text-align: center;
     padding: 1rem;
     color: var(--background-text);
-  }
-
-  .gradient {
-    background: linear-gradient(90deg, var(--primary), var(--secondary));
-    -webkit-background-clip: text;
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-    animation: gradient 5s ease-in-out infinite;
-    background-size: 200% 400%;
-  }
-
-  @keyframes gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
   }
 
   :global(.spin) {
@@ -400,100 +284,10 @@
     line-height: 1.5;
   }
 
-  .landing {
-    padding: 2rem;
-    color: var(--background-text);
-  }
-
-  h1 {
-    font-size: 6rem;
-    text-align: center;
-    font-weight: 900;
-  }
-
-  @media (max-width: 600px) {
-    h1 {
-      font-size: 4rem;
-    }
-  }
-
-  @media (max-width: 400px) {
-    h1 {
-      font-size: 3rem;
-    }
-  }
-
   h2 {
     font-size: 2.7rem;
     text-align: center;
     font-weight: 800;
     line-height: 1.2;
-  }
-
-  img {
-    display: block;
-    margin: 0 auto;
-    height: 25rem;
-  }
-
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  #upload {
-    text-align: center;
-    background-color: var(--primary);
-    color: var(--text);
-  }
-
-  .buttons {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--secondary);
-  }
-
-  .buttons .button {
-    position: relative;
-    display: flex;
-    border: none;
-    border-radius: 0;
-
-    cursor: pointer;
-
-    margin: 0;
-    padding: 0.5em 1.25em;
-    font-weight: 500;
-    font-size: var(--text-large);
-
-    color: inherit;
-    background-color: var(--secondary);
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.2em;
-
-    width: 14rem;
-    height: 100%;
-
-    font-weight: 400;
-  }
-
-  .buttons .button:disabled {
-    opacity: 0.5;
-  }
-
-  .buttons .button:hover {
-    background-color: var(--secondary-hover);
-  }
-
-  @media (min-width: 700px) {
-    .buttons {
-      flex-direction: row;
-    }
   }
 </style>
